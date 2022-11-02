4691f21f2da105dd32acd396acf2e163888ec80d
PtpJam <ruslan.fateev.rf@gmail.com>
2022-11-02 11:14:07 +0200

    Add Validation

---
+++ ActivationApi/Controllers/ActivationController.cs
@@ -0,0 +1,30 @@
+﻿using ActivationApi.Models;
+using ActivationApi.Models.Repos;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ActivationApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ActivationController : Controller
+    {
+        DeviceRepo repo;
+        public ActivationController()
+        {
+            repo = new DeviceRepo();
+        }
+        [HttpPost]
+        public IActionResult AddDevice(Device device)
+        {
+            var context = new ValidationContext(device);
+            var results = new List<ValidationResult>();
+            if (!Validator.TryValidateObject(device, context, results, true))
+                return StatusCode(400,  context.Items.First().Value); ;
+            repo.Add(device);
+            return StatusCode(200);
+        }
+        [HttpGet]
+        public IEnumerable<Device> Get() => repo.Get();
+    }
+}
--- ActivationApi/Models/Device.cs
+++ ActivationApi/Models/Device.cs
@@ -1,9 +1,16 @@
-﻿namespace ActivationApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ActivationApi.Models
 {
     public class Device
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Not valid Name (5-50 len)")]
         public string Name { get; set; }
+        [Required]
+        [StringLength(16, MinimumLength = 8, ErrorMessage = "Not valid IP (8-16  len)")]
         public string IP { get; set; }
     }
 }
